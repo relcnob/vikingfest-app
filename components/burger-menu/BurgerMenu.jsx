2b7e8b624fb020fea18994cd7f7fdee0e249340d
Fryderyk Boncler <f.relcnob@gmail.com>
2022-12-11 17:06:33 +0100

    added burger menu component

---
+++ components/burger-menu/BurgerMenu.jsx
@@ -0,0 +1,46 @@
+import styles from "./BurgerMenu.module.css";
+import Anchor from "../Anchor";
+import { useState } from "react";
+
+function BurgerMenu() {
+  const [menuState, setMenuState] = useState("inactive");
+
+  function updateState() {
+    menuState === "inactive"
+      ? setMenuState("active")
+      : setMenuState("inactive");
+  }
+
+  return (
+    <section className={` ${styles.burgerWrapper} `}>
+      <div
+        className={`${styles.burgerIcon} ${styles[menuState]}`}
+        onClick={() => {
+          updateState();
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`${styles.navWrapper} ${styles[menuState]}`}>
+        <ul>
+          <li className={styles.navItem}>
+            <Anchor href="#">My Ticket</Anchor>
+          </li>
+          <li className={styles.navItem}>
+            <Anchor href="#">Festival Map</Anchor>
+          </li>
+          <li className={styles.navItem}>
+            <Anchor href="#">Account</Anchor>
+          </li>
+          <li className={styles.navItem}>
+            <Anchor href="#">Contact</Anchor>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
+
+export default BurgerMenu;
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,3 +1,4 @@
+import BurgerMenu from "../../components/burger-menu/BurgerMenu";
 import SignupForm from "../../components/forms/signup-form/SignupForm";
 import NavBar from "../../components/nav-bar/NavBar";
 
@@ -5,7 +6,8 @@ function index() {
   return (
     <div style={{ background: "var(--primary-300)" }}>
       <SignupForm></SignupForm>
-      <NavBar active="bands" />
+      <NavBar active="schedule" />
+      <BurgerMenu />
     </div>
   );
 }
