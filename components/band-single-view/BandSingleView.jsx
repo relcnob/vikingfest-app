cd8dfd96e5619424738ac32d60b2b22eb96becf7
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-11 02:42:16 +0100

    final touches

--- components/band-single-view/BandSingleView.jsx
+++ components/band-single-view/BandSingleView.jsx
@@ -5,38 +5,38 @@ import MemberCard from "../MemberCard/MemberCard";
 
 function BandSingleView(props) {
   return (
-    <section className={styles.BandView}>
+    <article className={styles.BandView}>
       <div className="container">
+        <div className={styles.top}>
+          <button>{"<"}</button>
+          <Image alt="" className={styles.BandImage} src="/image.png" width="150" height="150" />
+        </div>
         <div>
-          <div className={styles.imgAndBtn}>
-            <button>{"<"}</button>
-            <Image alt="" className={styles.BandImage} src="/image.png" width="150" height="150" />
-          </div>
-          <div className={styles.h1AndStar}>
+          <div className={styles.title}>
             <h1>{props.bandName}</h1>
             <Image alt="star" />
           </div>
-          <section>
+          <section className={styles.description}>
             <h2>{props.genre}</h2>
             <p>{props.description}</p>
           </section>
-          <section className={styles.memberList}>
-            <h3>Members</h3>
-            <ul>
-              <li>
-                <MemberCard members={props.members} />
-              </li>
-              <li>
-                <MemberCard members={props.members} />
-              </li>
-              <li>
-                <MemberCard members={props.members} />
-              </li>
-            </ul>
-          </section>
         </div>
+        <section className={styles.memberList}>
+          <h3>Members</h3>
+          <ul>
+            <li>
+              <MemberCard members={props.members} />
+            </li>
+            <li>
+              <MemberCard members={props.members} />
+            </li>
+            <li>
+              <MemberCard members={props.members} />
+            </li>
+          </ul>
+        </section>
       </div>
-    </section>
+    </article>
   );
 }
 
