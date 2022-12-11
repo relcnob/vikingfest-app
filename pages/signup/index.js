6b07499969c47572ddc5b5548b5232beedefd595
Fryderyk Boncler <f.relcnob@gmail.com>
2022-12-11 15:17:24 +0100

    added navbar component, added icons for navbar added responsivity

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,9 +1,11 @@
 import SignupForm from "../../components/forms/signup-form/SignupForm";
+import NavBar from "../../components/nav-bar/NavBar";
 
 function index() {
   return (
     <div style={{ background: "var(--primary-300)" }}>
       <SignupForm></SignupForm>
+      <NavBar active="bands" />
     </div>
   );
 }
