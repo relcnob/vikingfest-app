a07206981c329e450af5f5cc3da4168295fa8dfc
relcnob <f.relcnob@gmail.com>
2024-04-18 00:46:21 +0200

    fixed image issue

--- components/band-overview/band-overview-genre/band-overview-item/BandOverviewItem.jsx
+++ components/band-overview/band-overview-genre/band-overview-item/BandOverviewItem.jsx
@@ -4,6 +4,17 @@ import Image from "next/image";
 import styles from "./BandOverviewItem.module.css";
 function BandOverviewItem(props) {
   const serverUrl = "https://vikingfest-api.onrender.com";
+
+  const getImage = () => {
+    if (props.logo.includes("placeimg")) {
+      return "https://via.assets.so/album.png?id=1&q=95&w=360&h=360&fit=fill";
+    } else {
+      return props.logo.includes("http")
+        ? props.logo
+        : `${serverUrl}/logos/${props.logo}`;
+    }
+  };
+
   return (
     <li>
       <Anchor href={`/bands/${props.slug}`} className={styles.wrapper}>
@@ -11,11 +22,7 @@ function BandOverviewItem(props) {
         <Image
           className={styles.img}
           alt={props.slug}
-          src={
-            props.logo.includes("http")
-              ? props.logo
-              : `${serverUrl}/logos/${props.logo}`
-          }
+          src={getImage()}
           width="256"
           height="256"
         />
